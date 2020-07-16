e23a9bb741a728f38d914aa709e0b06f71d38a49
xueno6 <45689247+xueno6@users.noreply.github.com>
2020-07-16 09:19:00 +0800

    Delete calculateDistance.m

--- calculateDistance.m
+++
@@ -1,4 +0,0 @@
-function distance = calculateDistance(target, duration, threshold, evalute)
-[CF, AF, PF, f, Fs, L] = fftAnalyze(target, duration, threshold);
-distance = evalute(CF, AF, PF, f, Fs, L);
-end
